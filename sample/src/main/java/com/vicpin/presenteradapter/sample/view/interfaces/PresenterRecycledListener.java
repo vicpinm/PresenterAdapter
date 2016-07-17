80a93393f155212292ddccf6c7e2493fc127001a
Victor Pineda <Victor Pineda>
2016-07-17 12:46:23 +0200

    listeners

---
+++ sample/src/main/java/com/vicpin/presenteradapter/sample/view/interfaces/PresenterRecycledListener.java
@@ -0,0 +1,9 @@
+package com.vicpin.presenteradapter.sample.view.interfaces;
+
+/**
+ * Created by Victor on 02/07/2016.
+ */
+public interface PresenterRecycledListener {
+
+    void onPresenterRecycled(int presenterId);
+}
