1cdf0e8813fe6999685d9d80a71c79e061d36c6f
Victor <victor.pineda@digio.es>
2016-09-19 17:32:48 +0200

    removed log

--- library/src/main/java/com/vicpin/presenteradapter/PresenterAdapter.java
+++ library/src/main/java/com/vicpin/presenteradapter/PresenterAdapter.java
@@ -21,7 +21,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -151,7 +150,6 @@ public abstract class PresenterAdapter<T> extends RecyclerView.Adapter<ViewHolde
 
     @Override
     public void onBindViewHolder(ViewHolder<T> holder, int position) {
-        Log.d("aa","bind position " + position + " size " + data.size());
         if(isNormalPosition(position)) {
             holder.onBind(data, getPositionWithoutHeaders(position));
             appendListeners(holder);
